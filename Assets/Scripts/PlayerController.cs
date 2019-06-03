50ad8c69b3d7b052d5a4366d9b680a9ae069b57f
hoshi_umeko <hoshi.kakiko@gmail.com>
2019-06-04 00:45:55 +0900

    回転と落下移動

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    private float timeOut = 1;
+    private float speed = 1;
+    private float timeElapsed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,21 +18,49 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        timeElapsed += Time.deltaTime;
+
+        if (timeElapsed >= timeOut * speed)
+        {
+            this.transform.Translate(0f, -1f, 0f, Space.World);
+
+            timeElapsed = 0.0f;
+        }
+
+        LeftMoveBlock();
+        RightMoveBlock();
+        RotateBlock();
+
+    }
+
+    private void LeftMoveBlock()
+    {
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            if(this.transform.position.x >= -5)
+            if (this.transform.position.x >= -5)
             {
-                this.transform.Translate(-1f, 0f, 0f);
+                this.transform.Translate(-1f, 0f, 0f, Space.World);
             }
         }
+    }
+
+    private void RightMoveBlock()
+    {
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             if (this.transform.position.x <= 5)
             {
-                this.transform.Translate(1f, 0f, 0f);
+                this.transform.Translate(1f, 0f, 0f, Space.World);
             }
         }
-        
     }
-        
+    
+    private void RotateBlock()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            this.transform.Rotate(0, 0, 90);
+        }
+
+    }
 }
