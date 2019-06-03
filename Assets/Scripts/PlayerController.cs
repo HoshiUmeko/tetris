40f768aaa1de24715c0bcc397b11861c9d3d861e
hoshi_umeko <hoshi.kakiko@gmail.com>
2019-06-03 23:58:23 +0900

    左右の移動を追加

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if(this.transform.position.x >= -5)
+            {
+                this.transform.Translate(-1f, 0f, 0f);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (this.transform.position.x <= 5)
+            {
+                this.transform.Translate(1f, 0f, 0f);
+            }
+        }
+        
+    }
+        
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject[] _block;
+
+    private int _num;
+    private float spawnTime = 3;
+    private float timeElapsed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BlockSpawer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void BlockSpawer()
+    {
+        _num = Random.Range(0, _block.Length);
+        GameObject BlockController = Instantiate(_block[_num], transform.position, transform.rotation);
+
+        BlockController.AddComponent<PlayerController>();
+    }
+}
